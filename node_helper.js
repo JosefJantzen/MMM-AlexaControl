766c70b82155ffca55f2bb0f6236a88f26ee6cb2
JoChef2 <45899513+JoChef2@users.noreply.github.com>
2020-06-03 18:30:30 +0200

    Edit cec-client

--- node_helper.js
+++ node_helper.js
@@ -259,14 +259,14 @@ module.exports = NodeHelper.create({
                     }
                 }
             }
-            else if(this.config.vcgencmd =='cec-client'){
+            else if(this.config.vcgencmd =='cec'){
                 device.handler = function(action) {     
                     if(action === 1){
-                        exec("echo 'on 0' | cec-client -s -d 1", opts, (error, stdout, stderr) => {
+                        exec("echo \'on 0.0.0.0\' | cec-client -s -d 1", opts, (error, stdout, stderr) => {
                             _this.checkForExecError(error, stdout, stderr);
                         });
                     }if(action === 0){
-                        exec("echo 'standby 0' | cec-client -s -d 1", opts, (error, stdout, stderr) => {
+                        exec("echo \'standby 0.0.0.0\' | cec-client -s -d 1", opts, (error, stdout, stderr) => {
                             _this.checkForExecError(error, stdout, stderr);
                         });
                     }
